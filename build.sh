96b8e0c07cc0db8ed349fdf4242b062226b731d6
Pavel Tsarenko <breitsmiley@gmail.com>
2019-07-06 01:05:46 +0300

    Release 0.0.2

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # DOCKER HUB Registry
 ##-------------------------------
-VERSION=0.0.1
+VERSION=0.0.2
 docker build -t breitsmiley/upw-rest-preprod:latest -t breitsmiley/upw-rest-preprod:${VERSION} . \
 && docker push breitsmiley/upw-rest-preprod:latest \
 && docker push breitsmiley/upw-rest-preprod:${VERSION}
